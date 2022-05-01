cc3caeac74c1b290f0bd92a33245ae0af9759477
Kartik Pandit <kpandit@umich.edu>
2022-04-30 23:46:20 -0500

    Add dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,51 @@
   import Keyboard from "./Keyboard.svelte";
 </script>
 
+<svelte:head>
+  <script src="./darkmode.js"></script>
+</svelte:head>
+
 <main>
-  <h1>Wordle</h1>
-  <Guesses />
-  <Keyboard />
+  <div class="container">
+    <header>
+      <h1>Wordle</h1>
+      <button id="dark-mode-toggle">☀️</button>
+    </header>
+    <Guesses />
+    <Keyboard />
+  </div>
 </main>
 
 <style>
   main {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  header {
+    display: flex;
+    justify-content: center;
+  }
+
+  .container {
     width: min(100% - 2rem, 600px);
     margin-inline: auto;
-    display: block;
+    display: flex;
+    gap: 1.5rem;
+    flex-direction: column;
     text-align: center;
   }
+
+  header > button {
+    max-width: max-content;
+    align-self: center;
+  }
+
   h1 {
-    margin: 20px 20px 20px 0;
-    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    /* margin: 12px 12px 12px 0; */
     font-size: 2rem;
     text-transform: uppercase;
   }
