f55da2bc2e6acc2966cdd09c10eef3f1d2537c6c
Kartik Pandit <kpandit@umich.edu>
2022-07-03 00:33:10 -0500

    Implement initial dialog

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,13 @@
   import Guesses from "./Guesses.svelte";
   import Keyboard from "./Keyboard.svelte";
   import settings from "../src/store/settings";
+  import store from "./store/index.js";
+  import  SvelteA11yDialog  from "svelte-a11y-dialog";
+
+  let dialogInstance;
+  const assignDialogInstance = (instance) => {
+    dialogInstance = instance;
+  };
 </script>
 
 <main>
@@ -10,12 +17,32 @@
       <h1>Wordle</h1>
       <button on:click={settings.toggleDarkMode}>☀️</button>
       <button on:click={settings.toggleContrast}>🌈</button>
-      <button on:click={settings.toggleHardMode}>
+      <button on:click={settings.toggleHardMode} disabled={$store.guessIdx > 0}>
         hard mode: {$settings.hardMode}
       </button>
     </header>
     <Guesses />
     <Keyboard />
+    <button type="button" data-a11y-dialog-show="a11y-dialog"
+      >Open dialog</button
+    >
+    <SvelteA11yDialog
+      id="a11y-dialog"
+      dialogRoot="#dialog-root"
+      closeButtonLabel="My close button label"
+      closeButtonPosition="last"
+      title="A11yDialog Test"
+      titleId="uniqueTitleId"
+      role="dialog"
+      on:instance={assignDialogInstance}
+    >
+      <svelte:fragment slot="closeButtonContent">
+        <span>Close</span>
+      </svelte:fragment>
+      <div>
+        <p>This is some content</p>
+      </div>
+    </SvelteA11yDialog>
   </div>
 </main>
 
