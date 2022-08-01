fe4d392831f71913ebcd6b76c9305eeca770ef94
Kartik Pandit <kpandit@umich.edu>
2022-07-31 23:26:41 -0500

    Switch backspace key label to unicode backspace character

--- src/Keyboard.svelte
+++ src/Keyboard.svelte
@@ -8,6 +8,8 @@
 
   $: currentGuess = $store.guesses[$store.guessIdx];
 
+  const backspaceKey = "\u232b";
+
   function handleSubmit() {
     // only consider valid words
     if (!isValidWord(currentGuess)) {
@@ -87,7 +89,7 @@
       });
     }
 
-    if (e.key === "Backspace" || e.key === "<-") {
+    if (e.key === "Backspace" || e.key === backspaceKey) {
       store.update((state) => {
         // prettier-ignore
         state.guesses[state.guessIdx] = state.guesses[state.guessIdx].slice(0, -1);
@@ -106,7 +108,7 @@
   {#each [ 
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Enter", "Z", "X", "C", "V", "B", "N", "M", "<-"]]
+    ["Enter", "Z", "X", "C", "V", "B", "N", "M", backspaceKey]]
     as row
   }
     <div class="row">
