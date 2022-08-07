82c6152a4e9b268f3c1114bd7ef2df1be918675c
Kartik Pandit <kpandit@umich.edu>
2022-08-06 22:57:47 -0500

    Connect refactored game state to guesses display

--- src/Guesses.svelte
+++ src/Guesses.svelte
@@ -1,20 +1,52 @@
 <script>
   import { getLetterType } from "../lib/helpers.js";
   import store from "./store/index.js";
-  import { solution } from "../lib/constants";
+  import { guesses, currentGuess } from "./store/game.js";
+  import { solution, NUM_GUESSES, WORD_LENGTH } from "../lib/constants";
+
+  $: numRemainingGuesses = Math.max(NUM_GUESSES - $guesses.length - 1, 0);
 </script>
 
 <div class="board">
-  {#each $store.guesses as guess, i}
-    <div class="row" class:completed={i < $store.guessIdx}>
-      <!-- make sure each col is rendered by padding '' to '      '-->
-      {#each guess.padEnd(5, " ") as letter, i}
+  {#each $guesses as guess, i}
+    <div class="row completed">
+      {#each guess as letter}
         <div class={`cell ${getLetterType(solution, guess, letter, i)}`}>
           {letter}
         </div>
       {/each}
     </div>
   {/each}
+
+  {#if $guesses.length !== NUM_GUESSES}
+    <div class="row">
+      {#each $currentGuess.padEnd(WORD_LENGTH) as letter}
+        <div class="cell">
+          {letter}
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  {#each Array(numRemainingGuesses).fill("") as blank}
+    <div class="row">
+      {#each $currentGuess.padEnd(WORD_LENGTH) as letter}
+        <div class="cell">
+          {blank}
+        </div>
+      {/each}
+    </div>
+  {/each}
+
+  <!-- {#each Array(NUM_GUESSES) as _, i}
+    <div class="row" class:completed={i < $guesses.length}>
+      {#each getRow(i) as letter, j}
+        <div class={`cell ${cellClass(i, letter)}`}>
+          {letter}
+        </div>
+      {/each}
+    </div>
+  {/each} -->
 </div>
 
 <style>
@@ -49,13 +81,13 @@
     10% {
       transform: translate(-2px, 0);
     }
-    
+
     20% {
       transform: translate(0, -2px);
     }
 
     30% {
-      transform: translate(0, 0);
+      transform: translate(2px, 0);
     }
 
     40% {
@@ -71,9 +103,9 @@
     background-color: var(--clr-correct);
   }
   .completed > .present {
-    background-color: var(--clr-present)
+    background-color: var(--clr-present);
   }
   .completed > .absent {
-    background-color: var(--clr-absent)
+    background-color: var(--clr-absent);
   }
 </style>
