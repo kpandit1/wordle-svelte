13b62954890963536d1e30c2cb455e7c6728b038
Kartik Pandit <kpandit@umich.edu>
2022-09-24 02:08:39 -0500

    🔧 Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["svelte3"],
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  ],
+  rules: {},
+};
