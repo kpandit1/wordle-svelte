5b6a35c1d55ff98bbda3dabb8f21201193827da0
Kartik Pandit <kpandit@umich.edu>
2023-06-11 01:55:48 -0500

    Fix feedback timing

--- src/lib/constants/animation.ts
+++ src/lib/constants/animation.ts
@@ -1,7 +1,7 @@
-import { MAX_NUM_GUESSES } from "./gameConstants";
+import { WORD_LENGTH } from "./gameConstants";
 
 export const ANIMATION_DELAY_MS: number = 250;
 export const CELL_ANIMATION_DURATION_MS: number = 650;
 
 export const TOTAL_ANIMATION_DURATION: number =
-  (MAX_NUM_GUESSES - 1) * ANIMATION_DELAY_MS + CELL_ANIMATION_DURATION_MS;
+  (WORD_LENGTH - 1) * ANIMATION_DELAY_MS + CELL_ANIMATION_DURATION_MS;
